6bf9c1cd0397f8a98cad8aae82dbc32546705ee0
caren-achieng <caren.achieng@strathmore.edu>
2022-02-18 23:24:15 +0300

    Home page and nav bar

---
+++ resources/views/includes/links.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="{{asset('vendor/bootstrap/bootstrap.min.css')}}">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Amatic+SC&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
